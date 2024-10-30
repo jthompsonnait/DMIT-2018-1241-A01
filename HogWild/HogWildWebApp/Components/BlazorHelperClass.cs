05e883ee55aaa22ccd371ca6bacad9819d9b1245
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2024-10-30 09:29:13 -0600

    End of Class Oct 30

---
+++ HogWild/HogWildWebApp/Components/BlazorHelperClass.cs
@@ -0,0 +1,14 @@
+﻿namespace HogWildWebApp.Components
+{
+    public static class BlazorHelperClass
+    {
+        //	Gets the Exception instance that caused the current exception.
+        //	An object that describes the error that caused the current exception.
+        public static Exception GetInnerException(System.Exception ex)
+        {
+            while (ex.InnerException != null)
+                ex = ex.InnerException;
+            return ex;
+        }
+    }
+}
--- HogWild/HogWildWebApp/Components/Pages/SamplePages/CustomerList.razor.cs
+++ HogWild/HogWildWebApp/Components/Pages/SamplePages/CustomerList.razor.cs
@@ -1,6 +1,124 @@
-﻿namespace HogWildWebApp.Components.Pages.SamplePages
+﻿using HogWildSystem.BLL;
+using HogWildSystem.ViewModels;
+using Microsoft.AspNetCore.Components.QuickGrid;
+using Microsoft.AspNetCore.Components;
+
+namespace HogWildWebApp.Components.Pages.SamplePages
 {
     public partial class CustomerList
     {
+        #region Fields
+
+        // The last name
+        private string lastName;
+
+        // The phone number
+        private string phoneNumber;
+
+        // The feedback message
+        private string feedbackMessage;
+
+        // The error message
+        private string errorMessage;
+
+        // has feedback
+        private bool hasFeedback => !string.IsNullOrWhiteSpace(feedbackMessage);
+
+        // has error
+        private bool hasError => !string.IsNullOrWhiteSpace(errorMessage);
+
+        // error details
+        private List<string> errorDetails = new();
+        #endregion
+
+        #region Properties
+        // Injects the CustomerService dependency.
+        [Inject]
+        protected CustomerService CustomerService { get; set; }
+
+        // Injects the NavigationManager dependency.
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        // Gets or sets the customers search view.
+        protected List<CustomerSearchView> Customers { get; set; } = new();
+
+        // Gets or sets the pagination with an initial value of 10 items per page.
+        protected PaginationState Pagination = new PaginationState { ItemsPerPage = 10 };
+        #endregion
+
+        //  search for an existing customer
+        private void Search()
+        {
+            try
+            {
+                //  reset the error detail list
+                errorDetails.Clear();
+
+                //  reset the error message to an empty string
+                errorMessage = string.Empty;
+
+                //  reset feedback message to an empty string
+                feedbackMessage = string.Empty;
+
+                //  clear the customer list before we do our search
+                Customers.Clear();
+
+                if (string.IsNullOrWhiteSpace(lastName) && string.IsNullOrWhiteSpace(phoneNumber))
+                {
+                    throw new ArgumentException("Please provide either a last name and/or phone number");
+                }
+
+                Customers = CustomerService.GetCustomers(lastName, phoneNumber);
+                if (Customers.Count > 0)
+                {
+                    feedbackMessage = "Search for customer(s) was successful";
+                }
+                else
+                {
+                    feedbackMessage = "No customer were found for your search criteria";
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                errorMessage = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            catch (ArgumentException ex)
+            {
+                errorMessage = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            catch (AggregateException ex)
+            {
+                //  have a collection of errors
+                //  each error should be place into a separate line
+                if (!string.IsNullOrWhiteSpace(errorMessage))
+                {
+                    errorMessage = $"{errorMessage}{Environment.NewLine}";
+                }
+                errorMessage = $"{errorMessage}Unable to search for customer";
+                foreach (var error in ex.InnerExceptions)
+                {
+                    errorDetails.Add(error.Message);
+                }
+            }
+        }
+
+        //  new customer
+        private void New()
+        {
+
+        }
+
+        //  edit selected customer
+        private void EditCustomer(int customerID)
+        {
+
+        }
+
+        //  new invoice for selected customer
+        private void NewInvoice(int customerID)
+        {
+
+        }
     }
 }
